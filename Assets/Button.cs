11bc0d12e3d1ab8c484e84fa5f15a367331a505b
LbsCristian <cristian.ingvarsson@elev.ga.lbs.se>
2024-04-23 15:05:54 +0200

    added more to the "level"

--- Assets/Button.cs
+++ Assets/Button.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     PolygonCollider2D pc;
-    Collider2D[] results= new Collider2D[4];
+    Collider2D[] results = new Collider2D[4];
     ContactFilter2D cc;
     public GameObject linkedObject;
-    
-    
+    public bool isToggle;
+    bool on;
+    bool touching;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +23,7 @@ public class Button : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (pc.OverlapCollider(cc,results) != 0)
+        if (on)
         {
             linkedObject.GetComponent<SpriteRenderer>().enabled = false;
             linkedObject.GetComponent<Collider2D>().enabled = false;
@@ -30,6 +33,40 @@ public class Button : MonoBehaviour
             linkedObject.GetComponent<SpriteRenderer>().enabled = true;
             linkedObject.GetComponent<Collider2D>().enabled = true;
         }
+
+        if (pc.OverlapCollider(cc, results) != 0)
+        {
+            if (isToggle&&!touching)
+            {
+                touching = true;
+                if (on)
+                {
+                    on = false;
+                }
+                else
+                {
+                    on = true;
+                }
+            }
+            else
+            {
+                on = true;
+            }
+        
+
+        }
+        else
+        {
+            if (!isToggle)
+            {
+                on = false;
+            }
+        }
     }
-    
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        touching = false;
+    }
 }
+    
+
